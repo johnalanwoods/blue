b955e5ef3c9549ada471aeba4915122342c45095
John Woods <john.alan.woods@gmail.com>
2018-10-04 17:05:57 +0100

    Initial Commit

---
+++ blue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  blue
+//
+//  Created by John Woods on 04/10/2018.
+//  Copyright © 2018 John Woods. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
