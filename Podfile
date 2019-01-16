208f7912fc2d22b84c9d597a99a4a0187b92d25d
John Woods <john.alan.woods@gmail.com>
2019-01-16 17:16:57 +0000

    v1.0

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+#platform :, '9.0'
+
+target 'blue' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RainbowSwift', '~> 3.0'
+  # Pods for blue
+
+end
--- blue/main.swift
+++ blue/main.swift
@@ -1,12 +1,58 @@
-//
-//  main.swift
-//  blue
-//
-//  Created by John Woods on 04/10/2018.
-//  Copyright © 2018 John Woods. All rights reserved.
-//
-
+import IOBluetooth
 import Foundation
+import RainbowSwift
 
-print("Hello, World!")
+// See https://developer.apple.com/reference/iobluetooth/iobluetoothdevice
+// for API details.
+class BluetoothDevices {
+	
+	var foundConnectedDevice = false
+	func pairedDevices() {
+		print("Getting connected bluetooth devices...".blue.bold)
+		guard let devices = IOBluetoothDevice.pairedDevices() else {
+			print("No devices.")
+			return
+		}
+		if (devices.count == 0) {
+			print("No devices.")
 
+		} else {
+			for (index, item) in devices.enumerated() {
+				if let device = item as? IOBluetoothDevice {
+					if (device.isConnected()) {
+						self.foundConnectedDevice = true
+						print("\(index+1)) \(device.name ?? "No name")")
+					}
+				}
+			}
+		}
+		if (self.foundConnectedDevice == false) {
+			print("No devices connected...".red)
+			print("\nAvailable Devices:".green.bold)
+
+			for (index , item) in devices.enumerated() {
+				if let device = item as? IOBluetoothDevice {
+					print("\(index+1)) \(device.name ?? "No name")")
+				}
+			}
+			print("\nConnect to device?".yellow.bold)
+			let inputNumber = Int(readLine()!)
+			if let inputNumber = inputNumber {
+				if let target = devices[inputNumber-1] as? IOBluetoothDevice {
+					let status = target.openConnection()
+					if(status == 0){
+						print("\(target.name ?? "")".green.bold, terminator: "")
+						print("connected.".green.bold.blink)
+					} else {
+						print("Connection to \(target.name ?? "")".red.bold, terminator: " ")
+						print("failed.".red.bold.blink)
+					}
+				}
+			}
+		}
+	}
+
+}
+
+var bt = BluetoothDevices()
+bt.pairedDevices()
